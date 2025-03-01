5c968d92c4fa37183d7f81790af4a99a1bbefd93
Rodrigo Gtz <66314699+Nesli22@users.noreply.github.com>
2025-03-01 02:43:48 -0700

    Set up initial project structure
    Initial commit with the basic project structure set up, including the main directories and files for the project.

---
+++ CoffeeLanguagePOS/src/main/java/util/ThemesViews.java
@@ -0,0 +1,37 @@
+
+package util;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class ThemesViews {
+
+    public static void setMacLightTheme() {
+        try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setLightTheme() {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setDarkTheme() {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
